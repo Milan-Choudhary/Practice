231ec18f28ff5ea02d4918fd0c61d555653943e2
Milan Choudhary <choudharymilan08@gmail.com>
2025-06-19 13:15:51 +0530

    Initial commit - greetings module assignment

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import * as math from './search.mjs';
+console.log(math.print('Milan Choudhary'));
+console.log(math.sum(10,10));
+console.log(math.sub(10,10));
+console.log(math.multiply(10,10));
+console.log(math.divide(10,10));
+
+//console.log("Let's see");
---
+++ search.mjs
@@ -0,0 +1,25 @@
+console.log('Hello, world!');
+
+export function print(name){
+    let msg = name + " " + 'Hello!';
+    return msg;
+}
+
+export function sum(a,b){
+    return a + b;
+}
+
+export function sub(a,b){
+    return a - b;
+}
+
+export function multiply(a,b){
+    return a*b;
+}
+
+export function divide(a,b){
+    return a/b;
+}
+
+console.log("Wonderful!");
+
